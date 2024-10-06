0c01911e3bb90623753f837f1ff0ce6360637600
ashjaei <ashjaei.developer@yahoo.com>
2024-10-06 09:11:30 +0330

    Fix:change route according to OSB requirements

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from orderPricePlan.routes import router as orderPricePlanRoutes
 app = FastAPI()
 
 
-app.include_router(orderPricePlanRoutes, prefix="/orderPricePlan", tags=["orderPricePlan"])
+app.include_router(orderPricePlanRoutes, prefix="/cbs/wrapper/orderPricePlanOffer", tags=["orderPricePlan"])
 
 
