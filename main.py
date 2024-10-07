8741316578a0771b64d869261d7a781b23a76a12
Mohammad Mostafavi <mohammad@mohammadmostafavi.com>
2024-10-07 22:07:03 +0330

    [ADD] payPostpaidHotBill

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from orderPricePlan.routes import router as orderPricePlanRoutes
 from recharge.routes import router as rechargesRoutes
 from queryPostpaidHotBill.routes import router as queryPostpaidHotBillRoutes
+from payPostpaidHotBill.routes import router as payPostpaidHotBillRoutes
 
 app = FastAPI()
 
@@ -9,6 +10,4 @@ app = FastAPI()
 app.include_router(orderPricePlanRoutes, prefix="/cbs/wrapper/orderPricePlanOffer", tags=["orderPricePlan"])
 app.include_router(rechargesRoutes, prefix="/cbs/wrapper/recharge", tags=["recharge"])
 app.include_router(queryPostpaidHotBillRoutes, prefix="/cbs/wrapper/queryPostpaidHotBill", tags=["queryPostpaidHotBill"])
-
-
-
+app.include_router(payPostpaidHotBillRoutes, prefix="/cbs/wrapper/payPostpaidHotBill", tags=["payPostpaidHotBill"])
---
+++ payPostpaidHotBill/handlers.py
@@ -0,0 +1,37 @@
+import os
+import xml.etree.ElementTree as ET
+from string import Template
+from datetime import datetime
+from utils.soap_client import soap_client
+from fastapi import HTTPException, Security, status
+
+
+def pay_postpaid_hot_bill_handler(data):
+    print('data:', {**data.__dict__})
+    print("datetime:",datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))
+    app_path = os.path.dirname(os.path.abspath(__file__))
+    with open(app_path+'/templates/payloads/Payment.txt', 'r') as file:
+        template = file.read()
+    template = Template(template)
+    dict_data = {**data.__dict__,"datetime":datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}
+    xml_data = template.substitute({k: v for k, v in dict_data.items() if v is not None})
+    print("request:", xml_data)
+    return soap_client.call_service('PayPostpaidHotBill', xml_data)
+
+
+
+def generate_response(response) :
+        root = ET.fromstring(response)
+        namespaces = {
+        'soapenv': 'http://schemas.xmlsoap.org/soap/envelope/',
+        'bcs': 'http://www.huawei.com/bme/cbsinterface/bcservices',
+        'cbs': 'http://www.huawei.com/bme/cbsinterface/cbscommon',
+        'bcc': 'http://www.huawei.com/bme/cbsinterface/bccommon'
+        }
+        balance = root.find('.//arc:NewBalanceAmt', namespaces)
+        if balance is not None:
+            return {
+                    "Balance": balance.text.strip()
+                }
+        
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
