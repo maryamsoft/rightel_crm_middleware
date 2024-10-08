2e06825866ed198c75c040772908ac12ce39a433
ashjaei <ashjaei.developer@yahoo.com>
2024-10-08 09:25:30 +0330

    Fix

--- orderPricePlan/routes.py
+++ orderPricePlan/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from .handlers import  generate_response, generate_response, ChangeSubOffering
 from .schemas import OrderPricePlanOfferRequest, ResponseBase
+from fastapi.responses import JSONResponse
 
 
 router = APIRouter()
@@ -10,9 +11,13 @@ async def orderPricePlanOffer(request: OrderPricePlanOfferRequest):
     try:
         CBS_response = ChangeSubOffering(request)
         result = generate_response(CBS_response)
-        response =  {
-            'OrderNbr':result
-        }
-        return response
+        if result:
+            response =  {
+                'OrderNbr':result
+            }
+            return response
+        return JSONResponse(content= {
+            'OrderNbr':None
+        })
     except Exception as error:
         raise error
