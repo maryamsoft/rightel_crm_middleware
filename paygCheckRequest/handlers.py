ce5375ddb0175ed826b9c4345200b2dfd4083a20
ashjaei <ashjaei.developer@yahoo.com>
2024-10-20 15:34:56 +0330

    Feat:PaygCheckRequest service

--- paygCheckRequest/handlers.py
+++ paygCheckRequest/handlers.py
@@ -2,15 +2,12 @@ import os
 import xml.etree.ElementTree as ET
 from jinja2 import Template
 from datetime import datetime
-from fastapi import HTTPException, status
+from fastapi import Response
 from utils.soap_client import BC_soap_client
-from utils import body 
 from .schemas import PaygCheckRequest
 
 
 def payg_check_request(data:PaygCheckRequest):
-    print('data:', {**data.__dict__})
-    print("datetime:",datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))
     app_path = os.path.dirname(os.path.abspath(__file__))
     with open(app_path+'/templates/payloads/QueryCustomerInfo.txt', 'r') as file:
         template = file.read()
@@ -21,10 +18,34 @@ def payg_check_request(data:PaygCheckRequest):
     }
     xml_data = template.render(**values)
     print("request:", xml_data)
-    return BC_soap_client.call_service('PaygCheckRequest', xml_data)
+    return BC_soap_client.call_service('QueryCustomerInfo', xml_data)
     
 
 def generate_response(cbs_response) :
-    pass
+    print('response:', cbs_response)
+    root = ET.fromstring(cbs_response)
+    namespaces = {
+    'soapenv': 'http://schemas.xmlsoap.org/soap/envelope/',
+    'bcs': 'http://www.huawei.com/bme/cbsinterface/bcservices',
+    'cbs': 'http://www.huawei.com/bme/cbsinterface/cbscommon',
+    'bcc': 'http://www.huawei.com/bme/cbsinterface/bccommon'
+    }
+
+    result_code = root.find('.//cbs:ResultCode', namespaces)
+    result_desc = root.find('.//cbs:ResultDesc', namespaces)
+    if result_code is not None and result_code.text == '0':
+        ATTRIBUTE_STATUS_title = root.find('.//bcc:Code', namespaces)
+        ATTRIBUTE_STATUS_value = root.find('.//bcc:Value', namespaces)
+        RESPONSE_CODE = result_code.text
+        sub_identity = root.find('.//bcc:SubIdentity', namespaces)
+        if sub_identity is not None:
+            subscriberNumber = sub_identity.find('bcc:SubIdentity', namespaces).text
+        return {
+                "attributeStatus":ATTRIBUTE_STATUS_value.text.strip(),
+                "responseDesc": "Successful",
+                "subscriberNumber": subscriberNumber,
+                "responseCode": RESPONSE_CODE
+            }
+    return None
 
 
