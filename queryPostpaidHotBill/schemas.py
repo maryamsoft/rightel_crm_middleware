b49a26bdadf7419217546b6316509924a1dad696
Mohammad Mostafavi <mohammad@mohammadmostafavi.com>
2024-10-25 13:05:29 +0330

    [FIX] QueryPostpaidHotBill

--- queryPostpaidHotBill/schemas.py
+++ queryPostpaidHotBill/schemas.py
@@ -1,4 +1,19 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 class QueryPostpaidHotBillRequest(BaseModel):
     msisdn: str
+
+
+class QueryPostpaidHotBillResponse(BaseModel):
+    PayableAmount: float
+    InvoiceID: str
+    PaymentID: str
+    Subscription: Optional[float]
+    AddSubscription: Optional[float]
+    TotalAmout: Optional[float]
+    Tax: Optional[float]
+    OutStanding: Optional[float]
+    BillingCycleStartDate: str
+    EndDate: str
