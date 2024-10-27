1b28a9e483774796ca2b5b658fb5f7bd82311534
Mohammad Mostafavi <mohammad@mohammadmostafavi.com>
2024-10-27 10:05:33 +0330

    [FIX] queryPostpaidHotBill

--- queryPostpaidHotBill/schemas.py
+++ queryPostpaidHotBill/schemas.py
@@ -3,7 +3,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 class QueryPostpaidHotBillRequest(BaseModel):
-    msisdn: str
+    MSISDN: str
 
 
 class QueryPostpaidHotBillResponse(BaseModel):
