931d73a54b7b157c6c2bcba4d505927af2bb77d7
ashjaei <ashjaei.developer@yahoo.com>
2024-10-30 15:33:36 +0330

    FIX:Debug paygChaeck

--- paygCheckRequest/handlers.py
+++ paygCheckRequest/handlers.py
@@ -22,7 +22,7 @@ def payg_check_request(data:PaygCheckRequest):
     
 
 def generate_response(cbs_response) :
-    print('response:', cbs_response)
+    print('cbs_response:', cbs_response)
     root = ET.fromstring(cbs_response)
     namespaces = {
     'soapenv': 'http://schemas.xmlsoap.org/soap/envelope/',
@@ -36,7 +36,7 @@ def generate_response(cbs_response) :
     if result_code is not None and result_code.text == '0':
         ATTRIBUTE_STATUS_title = root.find('.//bcc:Code', namespaces)
         ATTRIBUTE_STATUS_value = root.find('.//bcc:Value', namespaces)
-        RESPONSE_CODE = result_code.text
+        RESPONSE_CODE = result_code.text.strip()
         sub_identity = root.find('.//bcc:SubIdentity/bcc:SubIdentity', namespaces)
         print('sub_identity:', sub_identity)
         # if sub_identity is not None:
@@ -44,7 +44,7 @@ def generate_response(cbs_response) :
         return {
                 "attributeStatus":ATTRIBUTE_STATUS_value.text.strip(),
                 "responseDesc": "Successful",
-                "subscriberNumber": sub_identity,
+                "subscriberNumber": sub_identity.text.strip(),
                 "responseCode": RESPONSE_CODE
             }
     return None
