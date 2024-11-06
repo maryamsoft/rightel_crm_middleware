d2289a11e683c7796d0bb063049c7110ed90b6db
Mohammad Mostafavi <mohammad@mohammadmostafavi.com>
2024-11-06 11:14:28 +0330

    [FIX] RechargePPS

--- recharge/routes.py
+++ recharge/routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Response
-from .handlers import  recharge_handler, generate_response
+from .handlers import  recharge_handler, generate_response_normal, generate_response_exciting
 from .schemas import RechargeRequest
 from utils import header
 
@@ -9,7 +9,10 @@ router = APIRouter()
 async def recharge_pps(request: RechargeRequest, response: Response):
     try:
         xml_response = recharge_handler(request)
-        result = generate_response(xml_response)
+        if request.paymentType == "1":
+            result = generate_response_normal(xml_response)
+        else:
+            result = generate_response_exciting(xml_response)
         header.successful_header(response)
         return result
     except Exception as error:
