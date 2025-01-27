e59a214e8d33b33d67f691233058c34f167fa545
ashjaei <ashjaei.developer@yahoo.com>
2025-01-27 16:39:59 +0330

    recharge Exciting

--- recharge/routes.py
+++ recharge/routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Response, Header
-from .handlers import  recharge_handler, generate_response_normal, generate_response_exciting, generate_response
+from .handlers import  recharge_handler,generate_normal_response, generate_exciting_response
 from .schemas import RechargeRequest, CommonHeaders
 from utils import header
 from typing import Annotated
@@ -10,11 +10,11 @@ router = APIRouter()
 async def recharge_pps(request: RechargeRequest, response: Response):
     try:
         xml_response = recharge_handler(request)
-        result = generate_response(xml_response)
-        # if request.paymentType == "1":
-        #     result = generate_response_normal(xml_response)
-        # else:
-        #     result = generate_response_exciting(xml_response)
+        # result = generate_response(xml_response)
+        if request.paymentType == "1":
+            result = generate_normal_response(xml_response)
+        else:
+            result = generate_exciting_response(xml_response)
         header.successful_header(response)
         return result
     except Exception as error:
