c1ca5b63d0e32b0e03a605fe30f35b550220c074
ashjaei <ashjaei.developer@yahoo.com>
2025-02-04 11:02:25 +0330

    Feat: mainLog process

--- utils/logmodel.py
+++ utils/logmodel.py
@@ -1,4 +1,6 @@
 import json
+from utils.custom_handler import convert_uts_to_asia_tehran
+from datetime import datetime
 class logmodel:
     def __init__(self, ServiceUrl, RequestHeader,RequestBody,ResponsetHeader,ResponseBody,TimeSpan):
         self.ServiceUrl = ServiceUrl.__str__()
@@ -7,6 +9,16 @@ class logmodel:
         self.ResponsetHeader = ResponsetHeader.__str__()
         self.ResponseBody = ResponseBody.__str__()
         self.TimeSpan = TimeSpan/1000
+    def JsonString(self):
+        json_data = json.dumps(self.__dict__)
+        return json_data
+
+class mainLog:
+    def __init__(self,ServiceUrl, RequestHeader,RequestBody):
+        self.ServiceUrl = ServiceUrl.__str__()
+        self.RequestHeader = RequestHeader.__str__()
+        self.RequestBody = RequestBody.__str__()
+        self.RequestTime =  datetime.now().strftime("%Y-%m-%dT%H:%M:%S:%f")
     def JsonString(self):
         json_data = json.dumps(self.__dict__)
         return json_data
