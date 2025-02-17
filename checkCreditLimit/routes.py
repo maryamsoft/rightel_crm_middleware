399714f990fbe03bce5b688f30250e30c17b5f79
ashjaei <ashjaei.developer@yahoo.com>
2025-02-17 14:51:17 +0330

    Fix: add CM difference to log

--- checkCreditLimit/routes.py
+++ checkCreditLimit/routes.py
@@ -5,18 +5,21 @@ from fastapi.responses import JSONResponse
 from utils import header
 from utils.logmodel import CMLog
 from utils.logservice import logger
-
+from datetime import datetime
 
 router = APIRouter()
 
 @router.post('', response_model=CheckCreditLimitResponse)
 async def checkCreditLimit(request:CheckCreditLimitRequest ,  response: Response):
     try:
+        CM_request_time =  datetime.now()
         xml_response = check_credit_limit(request)
         result = generate_response(xml_response)       
         if result:
             header.successful_header(response)
-            logger.debug(CMLog(method = 'checkCreditLimit', ResponseBody=result).JsonString())
+            CM_response_time =  datetime.now()
+            difference=CM_response_time- CM_request_time
+            logger.debug(CMLog(method = 'checkCreditLimit', ResponseBody=result, CM_difference = difference).JsonString())
             return result
         
         header.unsuccessful_header(response)
--- querySubscriberRGUsage/routes.py
+++ querySubscriberRGUsage/routes.py
@@ -5,17 +5,22 @@ from fastapi.responses import JSONResponse
 from utils import header
 from utils.logmodel import CMLog
 from utils.logservice import logger
+from datetime import datetime
+
 
 router = APIRouter()
 
 @router.post('', response_model=QuerySubscriberRGUsageResponse)
 async def querySubscriberRGUsage(request:QuerySubscriberRGUsageRequest ,  response: Response):
     try:
+        CM_request_time =  datetime.now()
         xml_response = query_subscriber_rgusage(request)
         result = generate_response(xml_response)
         if result:
             header.successful_header(response)
-            logger.debug(CMLog(method = 'querySubscriberRGUsage', ResponseBody=result).JsonString())  
+            CM_response_time =  datetime.now()
+            difference=CM_response_time- CM_request_time
+            logger.debug(CMLog(method = 'querySubscriberRGUsage', ResponseBody=result, CM_difference = difference).JsonString())  
             return result
         
         return JSONResponse(content= {})
